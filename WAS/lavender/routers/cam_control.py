28cfff2a9477bf7366bcf07fad1e590d321cef72
JeongminYi <gginn1234@gmail.com>
2024-08-27 22:15:00 +0900

    lavender re_ID 이미지 출력 구현

--- WAS/lavender/routers/cam_control.py
+++ WAS/lavender/routers/cam_control.py
@@ -12,70 +12,71 @@ cam_control = Blueprint("cam_control", __name__)
 @cam_control.route("/device_registration", methods=["GET", "POST"])
 @login_required
 def register_device():
-    # 기기 등록
     if request.method == "POST":
-        network_ssid = request.form.get("network_ssid")  # 네트워크 이름(ssid)
+        # 네트워크 이름(ssid)
+        network_ssid = request.form.get("network_ssid").strip()
         session["network_ssid"] = network_ssid
-        network_security = request.form.get("network_security")  # 네트워크 암호화방식
+
+        # 네트워크 암호화방식
+        network_security = request.form.get("network_security").strip()
         session["network_security"] = network_security
-        network_pw = request.form.get("network_pw")  # 네트워크 비밀번호(password)
+
+        # 네트워크 비밀번호(password)
+        network_pw = request.form.get("network_pw").strip()
         session["network_pw"] = network_pw
-        user_id = current_user.get_id()  # user의 아이디
-        device_alias = request.form.get(
-            "device_alias"
-        )  # 기기 별칭(사용자 소유 기기 내 unique)
+
+        # user의 아이디
+        user_id = current_user.get_id().strip()
+
+        # 기기 별칭(사용자 소유 기기 내 unique)
+        device_alias = request.form.get("device_alias").strip()
         session["device_alias"] = device_alias
 
         # 유효성 검사
         qrcode_data = []
         qrcode_info = {}
-        if not network_ssid.strip():
-            flash("네트워크 이름이 공백입니다.")
+        qrcode_info["Network_SSID"] = network_ssid
+        qrcode_data.append(network_ssid)
+        qrcode_info["Security"] = network_security
+        qrcode_data.append(network_security)
+        qrcode_info["Network_PW"] = network_pw
+        qrcode_data.append(network_pw)
+        qrcode_info["Owner"] = user_id
+        qrcode_data.append(user_id)
+
+        device = Device.query.filter_by(
+            user_id=user_id, device_alias=device_alias
+        ).first()
+        if 1 <= len(network_pw) < 8:
+            flash(
+                "네트워크 비밀번호는 8자 이상 입력바랍니다.",
+                category="error",
+            )
+        elif device:
+            flash(
+                "보유 기기와 기기별칭이 중복됩니다.",
+                category="error",
+            )
+        elif not is_valid_device_alias(device_alias, 2, 17):
+            flash(
+                "기기별칭은 영문자, 숫자, '-', '_'만 사용가능하며, 2~17자 내로 입력바랍니다.",
+                category="error",
+            )
+        else:
+            qrcode_info["Device_Alias"] = device_alias
+            qrcode_data.append(device_alias)
+            # flash("정상적으로 QR코드가 생성되었습니다.", category="success")
+
             session.pop("network_ssid", None)
             session.pop("network_security", None)
             session.pop("network_pw", None)
-        elif not network_security:
-            flash("네트워크 암호화방식을 선택해주세요.", category="error")
-        else:
-            qrcode_info["Network SSID"] = network_ssid
-            qrcode_data.append(network_ssid)
-            qrcode_info["Security"] = network_security
-            qrcode_data.append(network_security)
-            qrcode_info["Network PW"] = network_pw
-            qrcode_data.append(network_pw)
-            qrcode_info["UserID"] = user_id
-            qrcode_data.append(user_id)
-            # qrcode_info["SECRET KEY"] = current_user.get_pw()
-            qrcode_data.append(current_user.get_pw())
+            session.pop("device_alias", None)
 
-            device = Device.query.filter_by(
-                user_id=user_id, device_alias=device_alias
-            ).first()
-            if device:
-                flash(
-                    "보유 기기와 기기별칭이 중복됩니다.",
-                    category="error",
-                )
-            elif not is_valid_device_alias(device_alias, 2, 17):
-                flash(
-                    "기기별칭은 영문자, 숫자, '-', '_'만 사용가능하며, 2~17자 내로 입력바랍니다.",
-                    category="error",
-                )
-            else:
-                qrcode_info["기기 별칭"] = device_alias
-                qrcode_data.append(device_alias)
-                flash("정상적으로 QR코드가 생성되었습니다.", category="success")
-
-                session.pop("network_ssid", None)
-                session.pop("network_security", None)
-                session.pop("network_pw", None)
-                session.pop("device_alias", None)
-
-                return render_template(
-                    "qrcode.html",
-                    qrcode_image=generate_qrcode(qrcode_data),
-                    qrcode_info=qrcode_info,
-                )
+            return render_template(
+                "qrcode.html",
+                qrcode_image=generate_qrcode(qrcode_data),
+                qrcode_info=qrcode_info,
+            )
 
     return render_template("device_registration.html")
 
--- WAS/lavender/routers/dashboard.py
+++ WAS/lavender/routers/dashboard.py
@@ -3,6 +3,9 @@ from flask_login import login_required, current_user
 from utils.paint_graph import get_all_basketball_play_shooting_mean_graph
 from models import GameInfo, UserInfo
 import matplotlib as plt
+import requests
+import os
+import base64
 
 dashboard = Blueprint("dashboard", __name__)
 
@@ -10,7 +13,12 @@ dashboard = Blueprint("dashboard", __name__)
 @dashboard.route("/dashboard")
 @login_required
 def show_dashboard():
-    games = GameInfo.query.filter_by(userID=current_user.get_id()).all()
+    try:
+        games = GameInfo.query.filter_by(userID=current_user.get_id()).all()
+    except Exception as ex:
+        # print(ex)
+        print("\nresult DB 연결 실패.")
+        return redirect(url_for("cam_control.show_devices"))
 
     result_games = []
     first = None
@@ -31,10 +39,21 @@ def show_dashboard():
 @dashboard.route("/dashboard_detail", methods=["GET", "POST"])
 @login_required
 def show_detail():
-    video_num = request.form.get("video_num")
-    fileName = request.form.get("fileName")
-    selected_games = GameInfo.query.filter_by(
-        userID=current_user.get_id(), video_num=video_num, fileName=fileName
-    ).all()
+    if request.method == "POST":
+        video_num = request.form.get("video_num")
+        fileName = request.form.get("fileName")
+        selected_games = GameInfo.query.filter_by(
+            userID=current_user.get_id(), video_num=video_num, fileName=fileName
+        ).all()
 
-    return render_template("dashboard_detail.html", games=selected_games)
+        return render_template(
+            "dashboard_detail.html",
+            games=selected_games,
+        )
+
+
+@dashboard.route("/players/<re_ID>")
+@login_required
+def get_image(re_ID):
+    res = requests.get(os.getenv("IMG_STORAGE_ADDR") + "/" + re_ID)
+    return res.content
